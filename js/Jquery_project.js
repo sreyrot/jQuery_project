243af44e1763e1b9e435b38f08f9f6ed447b3555
sreyrot.oun <sreyrot.oun@PNC.LAN>
2019-12-16 09:21:50 +0700

    this is calulate

--- js/Jquery_project.js
+++ js/Jquery_project.js
@@ -11,10 +11,19 @@ $(document).ready(() => {
         var recipeId = $("#recipe").val();
         selectRecipe(recipeId);
     });
-
-   
+    // calculate
+    $('#add').on('click', function(){
+        var input = $('#value').val();
+        userInput(input);
+        
+    })
+    $('#low').on('click', function(){
+        var input = $('#value').val();
+        lowInput(input);
+    })
 
 });
+
 // function to request api
 var requestApi = () => {
     $.ajax({
@@ -37,9 +46,9 @@ function getRecipe(data) {
     allData = data;
     var option = "";
     data.recipes.forEach(element => {
-      
+
         option += `
-    <option value ="${element.id}">${ element.name}</option>
+    <option value ="${element.id}">${element.name}</option>
     `;
     });
     $("#recipe").append(option);
@@ -50,7 +59,7 @@ function getRecipe(data) {
 // select recipi
 function selectRecipe(getID) {
     allData.recipes.forEach(item => {
-      
+
         if (item.id == getID) {
             eachRecipe(item.name, item.iconUrl);
             inGredient(item.ingredients);
@@ -70,7 +79,7 @@ function eachRecipe(name, iconUrl) {
     `;
     $("#name").html(names);
     // for iconUrl
-    var icons ="";
+    var icons = "";
     icons += `
     <img src="${iconUrl}" width="150px">
     `;
@@ -81,27 +90,27 @@ function eachRecipe(name, iconUrl) {
 function inGredient(ing) {
     var result = "";
     ing.forEach(item => {
-        
+
         result += `
         <tr>
             <td><img src="${item.iconUrl}" width="50px"></td>
-            <td>${item.quantity}</td>
+            <td id="put">${item.quantity}</td>
             <td>${item.unit[0]}</td>
             <td>${item.name}</td>
             
-
         </tr>
     `;
+    
     });
     $("#ing").html("Ingredient");
     $("#ingredient").html(result);
 }
 
 // introduction
-function getInstruction(int){
+function getInstruction(int) {
     var instruction = "";
     var splitInstruction = int.split("<step>");
-    for(let i = 1; i < splitInstruction.length; i++){
+    for (let i = 1; i < splitInstruction.length; i++) {
         instruction += `
            
             <h5 class="text-primary">Step: ${i}</h5>
@@ -111,4 +120,40 @@ function getInstruction(int){
     $("#ins").html("Instruction");
     $("#introduction").html(instruction);
 }
+
+// calculate
+
+function getNumberGest(nbGuests){
+    var cal = "";
+    cal +=`
+    <input type="text" id="value" value="${nbGuests}" class="form-control text-center" disabled>
+    `;
+    $('#input').html(cal);
+
+}
+
+function userInput(values){
+    var getValue = parseInt(values) + 1;
+    if(getValue <= 15){
+       $('#value').val(getValue);    
+       mal(getValue);
+    }
+}
+
+function lowInput(values){
+    var lowValue = parseInt(values) - 1;
+    if(lowValue >= 0  ){
+
+    $('#value').val(lowValue);
+    mal(lowValue);
+    }
+    
+}
+ function mal(output){
+ 
+     $("#put").html(output);
+
+  
+ }
+
 
